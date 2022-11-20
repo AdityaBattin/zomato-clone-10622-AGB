06a12b3defab5f287381652fe7188fb4d25834db
Maheshwari <97899505+MaheshwariSurwase@users.noreply.github.com>
2022-11-20 23:36:39 +0530

    intigrated redux in frontend

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import passport from "passport";
 import session from "express-session";
+import cors from 'cors'
+import helmet from 'helmet'
 
 // Private route authorization config
 import privateRouteConfig from "./config/route.config";
@@ -27,7 +29,8 @@ googleAuthConfig(passport);
 const zomato = express();
 
 // adding additional passport configuration
-
+zomato.use(cors({origin: "http://localhost:3000" }));
+zomato.use.apply(helmet());
 zomato.use(express.json());
 zomato.use(session({ secret: process.env.JWTSECRET }));
 zomato.use(passport.initialize());
